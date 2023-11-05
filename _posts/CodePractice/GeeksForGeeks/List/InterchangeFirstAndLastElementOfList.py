0b7faa3e175a5f46088c67cf4fd39bfae1437589
Pankaj Bhatt <pkj.25.bhatt@gmail.com>
2023-11-05 17:02:17 +0530

    completed

--- _posts/CodePractice/GeeksForGeeks/List/InterchangeFirstAndLastElementOfList.py
+++ _posts/CodePractice/GeeksForGeeks/List/InterchangeFirstAndLastElementOfList.py
@@ -13,6 +13,12 @@ def interchage1(inputList):
   inputList[0], inputList[-1]=inputList[-1], inputList[0]
   return inputList
 
+def interchange2(inputList):
+  '''using packing unpacking'''
+  a,*b,c = inputList
+  p,q = c,a
+  return [p,*b,q]
+
 if __name__ == '__main__':
   try:
     userList = [int(x) for x in input("enter comma separator integer value only =").split(',')]
@@ -28,6 +34,13 @@ if __name__ == '__main__':
 
     startTime = time.time()
     print(f'interchanged list using slice is = {interchage1(userList)}')
+    endTime = time.time()
+    print(f'timeComplexity = {endTime - startTime}')
+
+    print(f'Entered list is {userList}')
+
+    startTime = time.time()
+    print(f'interchanged list using slice is = {interchange2(userList)}')
     endTime = time.time()
     print(f'timeComplexity = {endTime - startTime}')
 
